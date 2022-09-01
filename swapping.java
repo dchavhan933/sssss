a4eecf9e590f8780960ed42e3977f0b813b86b8c
unknown <dchavhan933@gmail.com>
2022-09-01 21:34:40 +0530

    create new project

---
+++ swapping.java
@@ -0,0 +1,14 @@
+package devang;
+
+public class swapping {
+	public static void main(String[] args) {
+		int a=5;
+		int b=8;
+	
+		a=b;
+		System.out.println("a="+a);
+		b=b;
+		System.out.println("b="+b);
+	}
+
+}
